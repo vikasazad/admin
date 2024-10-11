d5c5919fc9e2273f36232a6fbd90e8d3ef99b19b
vikasazad <vikumar.azad@gmail.com>
2024-10-11 21:15:43 +0530

    11/10/2024

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    runtime: "edge",
+  },
+  // Enable SWC minification for improved performance
+  swcMinify: true,
+  // Disable React's static prerendering to use Edge Runtime
+  reactStrictMode: true,
+  // Optionally, you can add custom webpack configurations here
+  webpack: (config, { isServer }) => {
+    // Custom webpack configs if needed
+    return config;
+  },
+};
 
 export default nextConfig;
